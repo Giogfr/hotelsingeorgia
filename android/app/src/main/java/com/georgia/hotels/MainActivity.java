e5c61639b9f466877a6abd9408dc923aaa7c1466
Giogfr <idontsayidontsay@gmail.com>
2025-06-21 13:28:27 +0400

    Redesign home page for mobile

--- android/app/src/main/java/com/georgia/hotels/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.georgia.hotels;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
---
+++ android/app/src/main/java/com/georgia/stay/MainActivity.java
@@ -0,0 +1,5 @@
+package com.georgia.stay;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {} 
