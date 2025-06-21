46c767f96466421021701c1d3dec5a8fc3cdcc5f
Giogfr <idontsayidontsay@gmail.com>
2025-06-21 12:45:01 +0400

    Create Android build workflow

---
+++ android/app/src/main/java/com/georgia/hotels/MainActivity.java
@@ -0,0 +1,5 @@
+package com.georgia.hotels;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.georgia.hotels',
+  appName: 'Georgia Hotels',
+  webDir: 'out'
+};
+
+export default config;
